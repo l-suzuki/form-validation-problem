5f0e2093cad8fd655b86a8a6e27492f95f17e3fc
l-suzuki <lehsuzuki@gmail.com>
2018-06-07 11:04:21 +1200

    changed up the CSS to fit accessible contrast standards

--- components/Form.jsx
+++ components/Form.jsx
@@ -21,21 +21,28 @@ class Form extends React.Component {
 // update state to reflect what the user types into the form fields
   handleUserInput = (e) => {
     const name = e.target.name
+    const {errors} = this.state
     const value = e.target.value
     this.setState({
       [name]: value,
-      errors: []
+      errors: errors.filter(error => error != name)
     })
   }
 
 // updates animals array in state
   handleAnimalChoice = (e) => {
     const value = e.target.value
-    const {animals} = this.state
+    const {animals, errors} = this.state
     if (animals.includes(value)) {
-      this.setState({animals: [...animals].filter(animal => animal != value)})
+      this.setState({
+        animals: [...animals].filter(animal => animal != value),
+        errors: errors.filter(error => error != 'animal')
+      })
     } else {
-      this.setState({animals: [...animals, value]})
+      this.setState({
+        animals: [...animals, value],
+        errors: errors.filter(error => error != 'animal')
+      })
     }
   }
 
@@ -69,32 +76,21 @@ class Form extends React.Component {
       : ''
   }
 
-// displays error messages when incorrect data is entered.
-  displayErrorMessage = (error) => {
-    switch(error) {
-      case 'email': return 'Please enter a valid email address.'
-      case 'password': return 'Your password must have at least 8 characters.'
-      case 'colour': return 'Please select a colour.'
-      case 'animal': return 'You must select at least two animals.'
-      case 'tiger': return 'You must specify the type of tiger.'
-      default: return
-    }
-  }
-
   render(){
     const {email, password, colour, errors, valid} = this.state
     return (
       <div>
           <form method='post' action='' onSubmit={this.validate}>
-              <h1>Fill Out This Awesome Form!</h1>
-              <fieldset>
-                  <h3>Your details</h3>
+              <div className='h1'>Fill Out This Awesome Form!</div>
+              <fieldset className='fieldset'>
+                  <div className='h3'>Your Details</div>
                   <p className={this.addErrorClass('email')}>
                     <div className='details'>
                       <label className='label' htmlFor='email'>
                           Email
                       </label>
                       <input type='text' id='email' name='email' value={email} onChange={this.handleUserInput}></input>
+                      {errors.includes('email') && <p className='required' id='emailError'>*Please enter a valid email address.</p>}
                     </div>
                   </p>
                   <p className={this.addErrorClass('password')}>
@@ -102,16 +98,15 @@ class Form extends React.Component {
                       <label className='label' htmlFor='password'>
                           Password
                       </label>
-                      <input type='password' id='password' name='password' value={password} onChange={this.handleUserInput}></input>
-                      <p className='required'>*Your password must contain at least 8 characters.</p>
+                      <input type='password' id='password' name='password' placeholder='Include at least 8 characters' value={password} onChange={this.handleUserInput}></input>
+                      {errors.includes('password') && <p className='required' id='passwordError'>*Your password must contain at least 8 characters.</p>}
                     </div>
                   </p>
               </fieldset>
 
-              <fieldset>
-                  <h3>Your animal</h3>
+              <fieldset className='fieldset'>
+                  <div className='h3'>Your Animals</div>
                   <p className={this.addErrorClass('colour')}>
-                    <div className='animal'>
                       <label className='label' htmlFor='colour'>
                           Colour
                       </label>
@@ -123,12 +118,11 @@ class Form extends React.Component {
                           <option value='black'>Black</option>
                           <option value='brown'>Brown</option>
                       </select>
-                    </div>
+                      {errors.includes('colour') && <p className='required' id='colourError'>*Please select a colour.</p>}
                   </p>
                   <p className={this.addErrorClass('animal')}>
-                    <div className='animal'>
                       <span className="label">
-                          Animal
+                          Animals
                       </span>
 
                       <input type='checkbox' name='animal' value='bear' id='bear' onClick={this.handleAnimalChoice}></input>
@@ -150,26 +144,26 @@ class Form extends React.Component {
                       <label htmlFor='donkey'>
                            Donkey
                       </label>
-                    </div>
-                    <p className='required'>*You must select at least 2 animals.</p>
-
+                    {errors.includes('animal') && <p className='required' id='animalError'>*Please select at least 2 animals.</p>}
                   </p>
                   <p className={this.addErrorClass('tiger')}>
                     <div className='details'>
                       <label className='label' htmlFor='tiger_type'>
                           Type of tiger
                       </label>
-                      <input type='text' name='tiger_type' id='tiger_type' onChange={this.handleUserInput}></input>
-                      <p className='required'>*If you choose a tiger, please enter what type it is</p>
+                      <input type='text' name='tiger_type' id='tiger_type' placeholder='If you selected "tiger", please describe it here.' onChange={this.handleUserInput}></input>
+                      {errors.includes('tiger') && <p className='required' id='tigerError'>*Please enter the type of tiger.</p>}
                     </div>
                   </p>
               </fieldset>
-                {errors.length > 0 && <span id='errorDisplay'>{errors.map((error, i) => <p key={i} className='error'>{this.displayErrorMessage(error)}</p>)}</span>}
+                <div>
+                  {errors.length > 0 && <div className='error'>Oops! Something went wrong. Please ensure you have filled in everything correctly.</div>}
+                  {valid && <div id='success' className='h3'>Congratulations, you have submitted a valid form!</div>}
+                </div>
               <fieldset>
                   <p>
-                      <input type='submit' value='Create account'></input>
+                      <input type='submit' value='CREATE ACCOUNT'></input>
                   </p>
-                  {valid && <h1 id='success'>Congratulations, you have submitted a valid form!</h1>}
               </fieldset>
           </form>
       </div>
